07dad667e053bf634e391798ac22634466544b11
Andre Morryson <amorrysson77@gmail.com>
2024-03-15 19:55:50 -0300

    configurate tailwind and nativewindcss

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View,Text } from "react-native"
 
 export default function Home() {
     return (
-        <View>
-            <Text> alguma coisa funcioneee  </Text>
+        <View >
+            <Text>Macacos me mordam</Text>
         </View>
  )
 }
