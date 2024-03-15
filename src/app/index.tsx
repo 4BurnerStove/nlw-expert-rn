7e199c7a65cc61502bc44813d4062a2b6f959333
Andre Morryson <amorrysson77@gmail.com>
2024-03-15 19:38:24 -0300

    install tailwind and nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View,Text } from "react-native"
 export default function Home() {
     return (
         <View>
-            <Text> Alguma coisa </Text>
+            <Text> alguma coisa funcioneee  </Text>
         </View>
  )
 }
