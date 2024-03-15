e867341f834ee77ee436683db528e221cd95fe22
Andre Morryson <amorrysson77@gmail.com>
2024-03-15 20:08:32 -0300

    Added Slot and safeareaview to pass data from screen to screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View,Text } from "react-native"
 
 export default function Home() {
     return (
-        <View >
-            <Text>Macacos me mordam</Text>
+        <View className="flex-1 pt-8">
+            <Text className="text-white text-xl">Macacos me mordam</Text>
         </View>
  )
 }
