165f7b76d8f3cd03484e78feb741a433337a9a3d
Andre Morryson <amorrysson77@gmail.com>
2024-03-16 16:09:20 -0300

    initial stilizating header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
-import { View,Image } from "react-native";
+import { View, Image, Text } from "react-native";
 
 export function Header(){
     return (
-        <View className="Flex-row items-center border-b border-slate-700 pb-5 mx-5">
-            <Image source={require("@/assets/logo.png")} className="h-6 w-32"></Image>
+        <View className="flex-row items-center border-b border-slate-700 pb-5 mx-5">
+            <View className="flex-1">
+                <Image source={require("@/assets/logo.png")} className="h-6 w-32"></Image>
+                <Text className="text-white text-xl font-heading mt-1"> Cardápio </Text>
+            </View>
         </View>
     )
 }
