715e2aa816da5ce429b4bfbe5b61ffee5551b363
Andre Morryson <amorrysson77@gmail.com>
2024-03-17 20:08:23 -0300

    add FlatList and stylizate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
-import { View,Text } from "react-native"
+import { View, FlatList } from "react-native"
+
+import { CATEGORIES } from "@/utils/data/products"
 import { Header } from "../components/header"
 import { CategoryButton } from "../components/category-button"
 
@@ -6,11 +8,17 @@ export default function Home() {
     return (
         <View className="flex-1 pt-8"> 
             <Header title="Faça seu pedido" cartQuantityItems={1}/> 
-            <View className="flex-row gap-4">
-                <CategoryButton title="Lanche do dia"/>
-                <CategoryButton title="Lanche do dia"/>
-                <CategoryButton title="Lanche do dia"/>
-            </View>
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <CategoryButton title={item}/>
+                )}
+                horizontal
+                className="max-h-10 mt-5"
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
  )
 }
