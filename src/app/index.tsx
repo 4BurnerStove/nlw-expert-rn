278c0fed2cb29a099692ada3f8d411b395d6c812
Andre Morryson <amorrysson77@gmail.com>
2024-03-17 19:58:18 -0300

    install cslx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,8 +5,12 @@ import { CategoryButton } from "../components/category-button"
 export default function Home() {
     return (
         <View className="flex-1 pt-8"> 
-            <Header title="Faça seu pedido" cartQuantityItems={1}/>
-            <CategoryButton title="Lanche do dia"/>
+            <Header title="Faça seu pedido" cartQuantityItems={1}/> 
+            <View className="flex-row gap-4">
+                <CategoryButton title="Lanche do dia"/>
+                <CategoryButton title="Lanche do dia"/>
+                <CategoryButton title="Lanche do dia"/>
+            </View>
         </View>
  )
 }
--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -1,14 +1,17 @@
 import { Text, Pressable, PressableProps } from "react-native"
 
-type CategoryProps = {
+type CategoryProps = PressableProps & {
   title: string,
   isSelected?: boolean
 }
 
-export function CategoryButton({ title, isSelected }: CategoryProps){
+
+
+export function CategoryButton({ title, isSelected, ...rest }: CategoryProps){
   return (
-    <Pressable>
-      <Text>{title}</Text>
+    <Pressable className="bg-slate-800 px-4 justify-center rounded-md h-10" 
+    {...rest}>      
+      <Text className="text-slate-100 font-sub font-subtitle text-sm" >{title}</Text>
     </Pressable>
   )
 }
