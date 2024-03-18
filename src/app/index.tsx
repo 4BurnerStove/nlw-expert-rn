521a41e0297038e4f220441a1bbc6e4befe9cdb8
Andre Morryson <amorrysson77@gmail.com>
2024-03-18 10:53:54 -0300

    create product component and add Section list at home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,44 @@
-import { View, FlatList } from "react-native"
+import { useState } from "react"
+import { View, FlatList, SectionList, Text } from "react-native"
 
-import { CATEGORIES } from "@/utils/data/products"
+import { CATEGORIES, MENU } from "@/utils/data/products"
 import { Header } from "../components/header"
 import { CategoryButton } from "../components/category-button"
 
 export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+    }
+
     return (
         <View className="flex-1 pt-8"> 
-            <Header title="Faça seu pedido" cartQuantityItems={1}/> 
+            <Header title="Faça seu pedido" cartQuantityItems={7}/> 
             <FlatList
                 data={CATEGORIES}
                 keyExtractor={(item) => item}
                 renderItem={({ item }) => (
-                    <CategoryButton title={item}/>
+                    <CategoryButton title={item} 
+                    isSelected={item === category} 
+                    onPress={() => handleCategorySelect(item)}/>
                 )}
                 horizontal
                 className="max-h-10 mt-5"
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
                 showsHorizontalScrollIndicator={false}
+            />
+            <SectionList
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false} // para que nada fique por cima de nada
+                renderItem={({ item }) => (
+                    <Text className="text-white">{item.title}</Text>
+                )}
+                renderSectionHeader={({section: {title}}) => 
+                <Text className="text-xl text-white font-heading mt-8 mb-3">
+                    {title}
+                </Text>}
             />
         </View>
  )
---
+++ src/components/product.tsx
@@ -0,0 +1,17 @@
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+type ProductDataProps = {
+  title: string
+  description: string
+  thumbnail: string
+}
+type ProductProps = TouchableOpacityProps & {
+  data: ProductDataProps
+}
+
+export function Produt({ data, ...rest }: ProductProps) {
+  return (
+    <TouchableOpacity className="w-full flex-row items-center pb-4" {...rest}>
+    </TouchableOpacity>
+  )
+}
