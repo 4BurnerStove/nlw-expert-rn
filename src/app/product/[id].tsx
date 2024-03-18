dccb0d6b5c7bba52ff0c18b3de0dc1f402ba2c4d
Andre Morryson <amorrysson77@gmail.com>
2024-03-18 15:48:51 -0300

    add description and ingredients for tab products

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,8 @@
 import { View, Image, Text } from "react-native";
 import { useLocalSearchParams } from "expo-router" // recuperar ID dos produtos 
 import { PRODUCTS } from "@/src/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency"
+
 
 
 export default function Product(){
@@ -15,9 +17,18 @@ export default function Product(){
       source={product.cover} 
       className="w-full h-52" 
       resizeMode="cover"/>
-      <Text className="text-lime-400 text-2xl font-heading my-4">
-        {product.price}
-      </Text>
+      <View className="flex-1 p-4">
+        <Text className="text-lime-400 text-2xl font-heading my-4">
+          {formatCurrency(product.price)}
+        </Text>
+        <Text className="text-slate-400 font-body text-base leading-6  mb-6">
+          {product.description}
+        </Text>
+        {
+          product.ingredients.map((ingredient) => (
+            <Text key={ingredient} className="text-slate-400 text-base leading-6">{"\u2022"}{ingredient}</Text>
+          ))}
+      </View>
     </View>
   )
 }
